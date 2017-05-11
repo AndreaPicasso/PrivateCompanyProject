ec0e49eb6cdff40138b1de6042e6735b914ed79d
Simosound94 <simonemerello@hotmail.it>
2017-05-11 06:31:04 +0200

    commit

---
+++ app/core/lista-operatori/lista-operatori-component.js
@@ -0,0 +1,22 @@
+(function(angular) {
+'use strict';
+
+
+function ListaOperatoriController($scope, ListaOperatoriService) {
+  console.log("Lista Operatori ok");
+
+  this.operatori = [];
+  
+}
+    
+
+
+angular.
+  module('myApp', []).
+  component('listaOperatori', {
+    templateUrl: 'core/lista-operatori/lista-operatori-component.html',
+    controller: ListaOperatoriController
+
+  });
+})(window.angular);
+
