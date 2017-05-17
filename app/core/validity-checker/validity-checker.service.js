09da675db202da30ddc3b2df86c30ac509399cff
AndreaPicasso <andrea0pica@gmail.com>
2017-05-17 13:13:00 +0200

    Terminato la verifica correttezza manca solo dettagli sulla tutto collegato

--- app/core/validity-checker/validity-checker.service.js
+++ app/core/validity-checker/validity-checker.service.js
@@ -7,27 +7,42 @@
 angular.module('myApp').
   service('ValidityCheckerService', function(){
     this.almenoUnaSink=function(grafo){
-      var sink=grafo.getSinks();
-        // return sink!=[];
-        return false;
+      var sinks=grafo.getSinks();
+
+        return sinks!=[];
+        
     }
     this.almenoUnaSource=function(grafo){
-          var source=grafo.getSources();
-        // return source!=[];
-        return false;
+          var sources=grafo.getSources();
+
+        return sources!=[];
+        
     }
     this.controlloMolteplicita=function(grafo){
-        return false;
-        //da terminare usando il sequence diagramm rispettivo
+        var toCheck=grafo.getSinks();
+        var sources=grafo.getSources();
+        var i;
+        var sampleMolt=toCheck[0].molteplicita;
+        for(i=0; i<sources.length;i++ ){
+            if(sources[i].isOperatoreIO()){
+                toCheck.push(sources[i]);
+            }
+        }
+        for(i=0; i<toCheck.length;i++){
+            if(toCheck[i].molteplicita!=sampleMolt){
+                return false;
+            }
+        }
+        return true;
     }
     this.tuttoCollegato=function(grafo){
-    //   var link=grafo.getLinks();
-    //   var element=grafo.getElements();
-    //   var i;
-    //   var ports= new Array();
-    //   for(i=0; i<link.lenght(); i++){
-    //       ports[i]=element[i].getPorts();
-    //   }
+       var links=grafo.getLinks();
+      var element=grafo.getElements();
+      var i;
+      var ports= new Array();
+      for(i=0; i<links.length; i++){
+          ports[i]=element[i].getPorts();
+      }
       return false;
       //Simo termina qui guardando il tuo sequence diagram poi fammi ritornare un bool
     }
